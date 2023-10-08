0efd3de23658bd6df6845d4c91c9a9f27790fe36
Reazzzz <145277026+Reazzzz@users.noreply.github.com>
2023-10-08 17:59:56 +0300

    z2.py

---
+++ Занятие 2/z2.py
@@ -0,0 +1,22 @@
+#1
+#from math import*
+# x = float(input())
+# y = 0.75*10**-4
+# z = -0.845*10**2
+# s = (((9+((x-y)**2)) ** (1./3.)) / (x**2 + y**2 + 2)) - (exp(abs(x-y)) * tan(z)**3)
+# print(s)
+#2
+# from math import*
+# x = float(input())
+# y = 0.75*10**-4
+# z = -0.845*10**2
+# s = (((9+((x-y)**2)) ** (1./3.)) / (x**2 + y**2 + 2)) - (exp(abs(x-y)) * tan(z)**3)
+# print(s)
+
+#13
+from math import*
+x = 17.421
+y = 10.365*10**-3
+z = 0.828*10**5
+s = ((y+((x-1)**(1./3.)))**(1./4.)) / (abs(x-y) * (sin(z)**2 + tan(z)))
+print(s)
