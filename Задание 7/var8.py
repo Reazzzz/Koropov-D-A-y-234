de7b3aaf41fa9a5d35910cdc438e396c1aac6c16
Reazzzz <145277026+Reazzzz@users.noreply.github.com>
2023-10-16 08:43:14 +0300

    Create var8.py

---
+++ Задание 7/var8.py
@@ -0,0 +1,7 @@
+spisok = [1, 2, 3, 4, 5]
+summa = sum(spisok)
+proiz = 1
+for element in spisok:
+    proiz *= element
+print("Сумма элементов списка:", summa)
+print("Произведение элементов списка:", proiz)
