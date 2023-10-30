fda37ee899f0823fe4c3e7a3427c01e12420512f
Reazzzz <145277026+Reazzzz@users.noreply.github.com>
2023-10-30 08:43:26 +0300

    Update var8_1.py

--- Задание 8/var8_1.py
+++ Задание 8/var8_1.py
@@ -1,1 +1,12 @@
 
+def proverka(num):
+    digs = [int(dig) for dig in str(num)]
+    for dig in digs:
+        if dig == 0 or num % dig != 0:
+            return False
+    return True
+def poisk(n):
+    for num in range(1, n + 1):
+        if poisk(num):
+            print(num)
+poisk(100)
