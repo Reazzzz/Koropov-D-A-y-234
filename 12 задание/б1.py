7b3a25898deb00a77e8b869b0142b8e4c228e5b0
Reazzzz <145277026+Reazzzz@users.noreply.github.com>
2023-12-03 18:28:39 +0300

    Create б1.py

---
+++ 12 задание/б1.py
@@ -0,0 +1,17 @@
+def find_second_largest(numbers):
+    if len(numbers) == 0:
+        return None
+    max_num = max(numbers)
+    numbers.remove(max_num)
+    second_max = max(numbers)
+    return second_max
+numbers = []
+while True:
+    num = int(input("Введите число (0 для окончания ввода): "))
+    if num == 0:
+        break
+    numbers.append(num)
+second_largest = find_second_largest(numbers)
+print("Второй по величине элемент:", second_largest)
+
+input("Нажмите Enter для выхода")
